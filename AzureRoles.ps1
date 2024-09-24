f40d0491243b2de315ce912516dce9ca345bb7c5
ArneMagnussen <31843260+ArneMagnussen@users.noreply.github.com>
2024-09-24 22:06:55 +0200

    Add files via upload

--- AzureRoles.ps1
+++ AzureRoles.ps1
@@ -1,10 +1,15 @@
 # Azure cloud shell
 
+[CmdletBinding()] 
+param (
+    $ResellerAdminAgentID
+)
+
 $ErrorActionPreference = "Stop"
 
 # Reseller Admin Agent group ID --- Example dc2aa490-xxxx-yyyy-zzzz-02bcb829da93
 # Available in ACMP in Indirect Reseller Excperience Servcie
-$ResellerAdminAgentID = "dc2aa490-5b61-47a4-880a-02bcb829da93" # also software demo 
+# $ResellerAdminAgentID = "dc2aa490-5b61-47a4-880a-02bcb829da93" # also software demo 
 
 # AdminAgent Foreign Principal Group role to be assignet to Admin Agents --- Owner Role recommended
 $AssignRole = "Owner"
@@ -21,21 +26,11 @@ if (-not $ResellerAdminAgentID) {
 }
 
 
-#Clear-Variable $SubId
-#Clear-Variable $subArray
-#Clear-Variable $sublist
-#Clear-Variable $subsciptionsForUse
-#Clear-Variable $Subscriptions
-#Clear-Variable $SubscriptionIDs
-#Remove-Variable $subid¨ÅPOIUYTRE-AzSubscription
-
-#ALSO CSP NOK TenantID
-#$ALSOPACTenantID = "d1b209ea-0ffb-4ffe-b4dd-efac86a80e61" #NOK
 $ALSOPACAdminAgentID = "27679ace-cff1-4ba4-8c26-3f26c8b8c342" #NOK
 
 #Get all Azure subscriptions
 $subsciptions = Get-AzSubscription
-if (-not $subsciptions){
+if (-not $subsciptions) {
     Write-Host "No Azure Subscriptions found"
     Exit 1
 }
@@ -52,39 +47,66 @@ foreach ($sub in $subArray) {
     $subsciptionsForUse += $subsciptions[$sub - 1]
 }
 
-# AdminAgent GroupIDs to assign FPG permissions
-$AdminAgentIDs =@(
+<# AdminAgent GroupIDs to assign FPG permissions
+$AdminAgentIDs = @(
     $ResellerAdminAgentID
     $ALSOPACAdminAgentID)
-$result =  @()
+#>
+
+$AdminAgentIDs = @()
+$AdminAgentIDs += New-Object -TypeName psobject -Property @{Partner="ALSO"; ID=$ALSOPACAdminAgentID}
+$AdminAgentIDs += New-Object -TypeName psobject -Property @{Partner="Partner"; ID=$ResellerAdminAgentID}
+
+$result = @()
+$row = @()
 
 foreach ($SubId in $subsciptionsForUse) {
     foreach ($Gid in $AdminAgentIDs) {
-        try{ 
-            New-AzRoleAssignment -ObjectId $Gid -RoleDefinitionName $AssignRole -Scope /subscriptions/$SubId -ObjectType ForeignGroup | Out-Null
+        try { 
+            New-AzRoleAssignment -ObjectId $Gid.ID -RoleDefinitionName $AssignRole -Scope /subscriptions/$SubId -ObjectType ForeignGroup | Out-Null
             $row = [PSCustomObject]@{
                 SubscriptionName = $SubId.name
                 Status           = "Role Assigned"
                 AssignedRoles    = $AssignRole
-                GroupID          = $Gid}
-                $AssignedRoles++
+                Assignee         = $Gid.Partner
+            }
+            $AssignedRoles++
         }
         catch {
-            $row = [PSCustomObject]@{
-                SubscriptionName = $SubId.name
-                Status           = $_.Exception.Message
-                AssignedRoles    = ""
-                GroupID          = ""}
+            try {
+                $RolesAssigned = Get-AzRoleAssignment -Scope /subscriptions/$subid
+                if ($RolesAssigned | where-object { $_.RoleDefinitionName -eq $AssignRole -and $_.objectId -eq $Gid.ID }) {
+                    $row = [PSCustomObject]@{
+                        SubscriptionName = $SubId.name
+                        Status           = "Role Present"
+                        AssignedRoles    = "Owner"
+                        Assignee         = $Gid.Partner
+                    }  
+                } 
+            }
+            catch {
+                $row = [PSCustomObject]@{
+                    SubscriptionName = $SubId.name
+                    Status           = $_.Exception.Message
+                    AssignedRoles    = ""
+                    Assignee         = $Gid.Partner
+                }
+            }
         }
         $result += $row
     }
 }
-$result  | Format-List
 
 $result  | Format-Table
 
 
-<# 
-$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/ArneMagnussen/AzureRoles/AzureRoles.ps1
+<#
+$AdminAgentID="dc2aa490-5b61-47a4-880a-02bcb829da93"
+$ScriptFromGitHub = Invoke-WebRequest "https://raw.githubusercontent.com/ArneMagnussen/AzureRoles/refs/heads/main/AzureRoles.ps1 -ResellerAdminAgentID $AdminAgentID"
 Invoke-Expression $($ScriptFromGitHub.Content)
+
+https://raw.githubusercontent.com/ArneMagnussen/AzureRoles/edit/main/AzureRoles.ps1
+https://raw.githubusercontent.com/ArneMagnussen/AzureRoles/refs/heads/main/AzureRoles.ps1
+
+AzureRoles.ps1
 #>
