69d7ece359435e2ccc13667232908f94cc330554
ArneMagnussen <31843260+ArneMagnussen@users.noreply.github.com>
2024-09-30 10:59:55 +0200

    Add files via upload

--- ps1/AzureRoles.ps1
+++ ps1/AzureRoles.ps1
@@ -105,11 +105,11 @@ $result  | Format-Table
 <#
 Admin Agent as Argument
 $AdminAgentID="dc2aa490-5b61-47a4-880a-02bcb829da93"
-$Script = Invoke-WebRequest https://raw.githubusercontent.com/ArneMagnussen/AzureRoles/refs/heads/main/AzureRoles.ps1
+$Script = Invoke-WebRequest https://raw.githubusercontent.com/ArneMagnussen/AzureRoles/refs/heads/main/ps1/AzureRoles.ps1
 $ScriptBlock = [Scriptblock]::Create($Script.Content)
 Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList ($args + @($AdminAgentID))
 
 Admin Agent prompted
-$Script = Invoke-WebRequest https://raw.githubusercontent.com/ArneMagnussen/AzureRoles/refs/heads/main/AzureRoles.ps1
+$Script = Invoke-WebRequest https://raw.githubusercontent.com/ArneMagnussen/AzureRoles/refs/heads/main/ps1/AzureRoles.ps1
 Invoke-Expression $($script.content)
 #>
